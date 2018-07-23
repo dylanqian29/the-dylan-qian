d1e2f1967ce86a8e7b1e3c380b7a2eaca6c930d9
Dylan Qian <dylan.qian.29@gmail.com>
2018-07-22 23:48:46 -0700

    finished modal, and done with first draft, wait for deploy to heroku

--- src/components/Background.js
+++ src/components/Background.js
@@ -10,16 +10,16 @@ class Background extends Component {
         <div class="section__title">Background</div>
         <div class="section__content">
           <p>
-            I'm currently an Software Engineer and Teaching Associate at <a class="underline-link" href="https://www.hackreactor.com/" > Hack Reactor</a>  building things with awesome people and mentoring new engineers. I graduated from
+            I'm currently a Software Engineer and Teaching Associate at <a class="underline-link" href="https://www.hackreactor.com/" > Hack Reactor</a>  building things with awesome people and mentoring new engineers. I graduated from
             <a class="underline-link" href="https://msem.engineering.jhu.edu/">Johns Hopkins for M.S in Engineering Management Program</a> in 2017.
             After pursuing business career as a Business Consultant at <a class="underline-link" href="http://www.unitedamgpartners.com/">United AMG Partners</a>, 
-            I found my passion in creating and optimizing web, what I always want to do during college and graduate school.
+            I found my passion in creating and optimizing web, what I has always been somethings I wanted to do during college and graduate school.
           </p>
 
           <p>
             As a software engineer, I enjoy bridging the gap between engineering and business &mdash; combining my technical knowledge
             and business background to create applications that can bring people conveninence. Even the tech industry is getting more satured, 
-            but I always believe there are tons of <a class="underline-link" href="https://en.wikipedia.org/wiki/Blue_Ocean_Strategy">Blue Oceans</a> out there for me to discover.
+            I always believe that there are tons of <a class="underline-link" href="https://en.wikipedia.org/wiki/Blue_Ocean_Strategy">Blue Oceans</a> out there for me to discover.
           </p>
 
           <p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
 import '../scss/global.scss';
 import '../scss/Projects.scss';
+import Modal from "./Modal.js"
 
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      id: null
+    }
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+
+  }
+
+  openModal(id) {
+    this.setState({
+      isOpen: true,
+      id:id,
+    })
+  }
+
+  closeModal() {
+    this.setState({
+      isOpen: false,
+      id:null
+    })
+  }
+
+ 
+
   render() {
     return (
       <section class="section featured-projects">
+        {this.state.isOpen && <Modal id={this.state.id} closeModal={this.closeModal}/>}
         <div class="section__title"> Projects</div>
           <div class="section__content">
-
-
             <div class="project welp">
               <figure class="project__pic">
-              <a href="https://www.youtube.com/watch?v=sxe5fRYas0c&amp;feature=youtu.be" >
-                  <img src="https://s3-us-west-1.amazonaws.com/thedylanqian/welp.png" alt=""/>
+                <a >
+                  <img onClick={() => this.openModal("sxe5fRYas0c")} src="https://s3-us-west-1.amazonaws.com/thedylanqian/welp.png" alt=""/>
                 </a>
               </figure>
               <figcaption class="project__caption">
                   <div class="project__name">
-                <a href="https://www.youtube.com/watch?v=sxe5fRYas0c&amp;feature=youtu.be"> Welp </a>
+                <a onClick={() => this.openModal("sxe5fRYas0c")}> Welp </a>
                   </div>
                 <p>Reviews app for local businesses and restaurants using ReactJS, Node.js, Express.js, MongoDB, Redis, Docker, Jest, Enzyme, Flexbox</p>
               </figcaption>
@@ -28,13 +55,13 @@ class Projects extends Component {
 
             <div class="project dreambnb">
               <figure class="project__pic">
-                <a href="" >
+                <a >
                   <img src="https://s3-us-west-1.amazonaws.com/thedylanqian/dreambnb.png" alt="" />
                 </a>
               </figure>
               <figcaption class="project__caption">
                 <div class="project__name">
-                  <a href=""> Dreambnb </a>
+                  <a> Dreambnb </a>
                 </div>
               <p>Optimization for back-end architecture to handle heavy traffic and ensure scalability using ReactJS, Node.js, GraphQL, Server-Side Rendering, Artillery.io, New Relic</p>
               </figcaption>
@@ -43,13 +70,13 @@ class Projects extends Component {
 
             <div class="project dancein">
               <figure class="project__pic">
-                <a href="https://www.youtube.com/watch?v=jKaH6Mbl1G8" >
-                  <img src="https://s3-us-west-1.amazonaws.com/thedylanqian/dancein.png" alt="" />
+                <a >
+                <img onClick={() => this.openModal("jKaH6Mbl1G8")} src="https://s3-us-west-1.amazonaws.com/thedylanqian/dancein.png" alt="" />
                 </a>
               </figure>
               <figcaption class="project__caption">
                 <div class="project__name">
-                  <a href="https://www.youtube.com/watch?v=jKaH6Mbl1G8"> DanceIn </a>
+                <a onClick={() => this.openModal("jKaH6Mbl1G8")}> DanceIn </a>
                 </div>
                 <p> Social network and events app for dancers using React Router, Passport.js, Bootstrap</p>
               </figcaption>
