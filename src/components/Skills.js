eb5d6bf6c26148648ea2e3c9a214e9a0febe5fbe
Dylan Qian <dylanqian@Dylans-MBP.attlocal.net>
2021-09-12 15:00:14 -0700

    updated skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,22 +14,21 @@ class Skills extends Component {
               <div className="skillz__category__label">Languages</div>
               <ul>
                 <li className="skillz__category__item"> <i className="devicon-javascript-plain"></i> JavaScript (ES6)</li>
+                <li className="skillz__category__item"> <i className="devicon-ruby-plain"></i> Ruby </li>
                 <li className="skillz__category__item"> <i className="devicon-html5-plain"></i> HTML5</li>
                 <li className="skillz__category__item"> <i className="devicon-css3-plain"></i> CSS3</li>
                 <li className="skillz__category__item"> <i className="devicon-sass-original"></i> Sass</li>
                 <li className="skillz__category__item"> <i className="devicon-python-plain"></i> Python</li>
-                <li className="skillz__category__item"> <i className="devicon-mysql-plain"></i> SQL</li>
-                <li className="skillz__category__item"> <img src="https://s3-us-west-1.amazonaws.com/thedylanqian/r.png" valign="top" width="14.39" height="12" alt="" /> R</li>
+                <li className="skillz__category__item"> <i className="devicon-postgresql-plain"></i> SQL</li>
               </ul>
             </div>
             <div className="skillz__category">
               <div className="skillz__category__label">Frameworks</div>
               <ul>
                 <li className="skillz__category__item"> <i className="devicon-react-original"></i> React</li>
-                <li className="skillz__category__item"> <i className="devicon-angularjs-plain"></i> AngularJS</li>
-                <li className="skillz__category__item"> <i className="devicon-backbonejs-plain"></i> BackBoneJS</li>
                 <li className="skillz__category__item"> <i className="devicon-nodejs-plain"></i> Node.js</li>
                 <li className="skillz__category__item"> <i className="devicon-express-original"></i> Express</li>
+                <li className="skillz__category__item"> <i className="devicon-rails-plain"></i> Ruby on Rails</li>
                 <li className="skillz__category__item"> <i className="devicon-mocha-plain"></i> Mocha</li>
                 <li className="skillz__category__item"> <i className="devicon-jasmine-plain"></i> Jasmine</li>
               </ul>
@@ -42,10 +41,8 @@ class Skills extends Component {
                 <li className="skillz__category__item"> <i className="devicon-docker-plain"></i> Docker</li>
                 <li className="skillz__category__item"> <i className="devicon-heroku-original"></i> Heroku</li>
                 <li className="skillz__category__item"> <i className="devicon-redis-plain"></i> Redis</li>
-                <li className="skillz__category__item"> <i className="devicon-jquery-plain"></i> JQuery</li>
                 <li className="skillz__category__item"> <i className="devicon-bootstrap-plain"></i> Bootstrap</li>
                 <li className="skillz__category__item"> <i className="devicon-babel-plain"></i> Babel</li>
-
               </ul>
             </div>
             <div className="skillz__category">
