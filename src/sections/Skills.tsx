5874e12f36884233532a522f25b41f51429b42ff
Dylan Qian <dylan.qian.29@gmail.com>
2024-04-24 15:26:57 -0700

    fixed all spacing

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -29,7 +29,7 @@ const SkillsCategoryItem = styled.li`
 `;
 
 const SkillLogo = styled.i`
-  margin-right: 5px;
+  margin-right: 8px;
 `;
 
 const PostmanLogo = styled.img`
@@ -51,20 +51,20 @@ const Skills = () => (
               JavaScript
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-html5-plain"></SkillLogo> HTML5
+              <SkillLogo className="devicon-html5-plain"></SkillLogo>HTML5
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-css3-plain"></SkillLogo> CSS3
+              <SkillLogo className="devicon-css3-plain"></SkillLogo>CSS3
             </SkillsCategoryItem>
             <SkillsCategoryItem>
               <SkillLogo className="devicon-tailwindcss-original"></SkillLogo>
               Tailwind
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-postgresql-plain"></SkillLogo> SQL
+              <SkillLogo className="devicon-postgresql-plain"></SkillLogo>SQL
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-python-plain"></SkillLogo> Python
+              <SkillLogo className="devicon-python-plain"></SkillLogo>Python
             </SkillsCategoryItem>
           </ul>
         </SkillsCategory>
@@ -72,7 +72,7 @@ const Skills = () => (
           <SkillsCeteogryLabel>Frameworks</SkillsCeteogryLabel>
           <ul>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-react-original"></SkillLogo> React
+              <SkillLogo className="devicon-react-original"></SkillLogo>React
             </SkillsCategoryItem>
             <SkillsCategoryItem>
               <SkillLogo className="devicon-reactrouter-plain"></SkillLogo>
@@ -83,14 +83,14 @@ const Skills = () => (
               Bootstrap
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-nodejs-plain"></SkillLogo> Node.js
+              <SkillLogo className="devicon-nodejs-plain"></SkillLogo>Node.js
             </SkillsCategoryItem>
             <SkillsCategoryItem>
               <SkillLogo className="devicon-express-original"></SkillLogo>
               Express
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-jest-plain"></SkillLogo> Jest
+              <SkillLogo className="devicon-jest-plain"></SkillLogo>Jest
             </SkillsCategoryItem>
           </ul>
         </SkillsCategory>
@@ -98,11 +98,11 @@ const Skills = () => (
           <SkillsCeteogryLabel>Tools</SkillsCeteogryLabel>
           <ul>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-amazonwebservices-plain-wordmark"></SkillLogo>{" "}
+              <SkillLogo className="devicon-amazonwebservices-plain-wordmark"></SkillLogo>
               AWS
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-docker-plain"></SkillLogo> Docker
+              <SkillLogo className="devicon-docker-plain"></SkillLogo>Docker
             </SkillsCategoryItem>
             <SkillsCategoryItem>
               <SkillLogo className="devicon-netlify-plain"></SkillLogo>
@@ -113,10 +113,10 @@ const Skills = () => (
               Vercel
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-redis-plain"></SkillLogo> Redis
+              <SkillLogo className="devicon-redis-plain"></SkillLogo>Redis
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-postman-plain"></SkillLogo> Postman
+              <SkillLogo className="devicon-postman-plain"></SkillLogo>Postman
             </SkillsCategoryItem>
           </ul>
         </SkillsCategory>
@@ -124,22 +124,22 @@ const Skills = () => (
           <SkillsCeteogryLabel>Others</SkillsCeteogryLabel>
           <ul>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-git-plain"></SkillLogo> Git
+              <SkillLogo className="devicon-git-plain"></SkillLogo>Git
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-github-plain"></SkillLogo> Github
+              <SkillLogo className="devicon-github-plain"></SkillLogo>Github
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-vscode-plain"></SkillLogo> VS Code
+              <SkillLogo className="devicon-vscode-plain"></SkillLogo>VS Code
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-vite-original"></SkillLogo> Vite
+              <SkillLogo className="devicon-vite-original"></SkillLogo>Vite
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-nextjs-plain"></SkillLogo> Next.js
+              <SkillLogo className="devicon-nextjs-plain"></SkillLogo>Next.js
             </SkillsCategoryItem>
             <SkillsCategoryItem>
-              <SkillLogo className="devicon-mongodb-plain"></SkillLogo> MongoDB
+              <SkillLogo className="devicon-mongodb-plain"></SkillLogo>MongoDB
             </SkillsCategoryItem>
           </ul>
         </SkillsCategory>
